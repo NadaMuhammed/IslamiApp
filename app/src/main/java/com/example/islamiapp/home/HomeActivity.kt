1ecfb5be63a6b3787c352d7048284c2794565800
NadaMuhammed <nadamuhammed776@gmail.com>
2024-01-15 10:11:29 +0200

    Finished bottom navigation view.

---
+++ app/src/main/java/com/example/islamiapp/home/HomeActivity.kt
@@ -0,0 +1,47 @@
+package com.example.islamiapp.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.islamiapp.R
+import com.example.islamiapp.databinding.ActivityHomeBinding
+import com.example.islamiapp.home.screens.AhadethFragment
+import com.example.islamiapp.home.screens.QuranFragment
+import com.example.islamiapp.home.screens.RadioFragment
+import com.example.islamiapp.home.screens.TasbeehFragment
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    val radioFragment = RadioFragment()
+    val tasbeehFragment = TasbeehFragment()
+    val ahadethFragment = AhadethFragment()
+    val quranFragment = QuranFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setCurrentFragment(radioFragment)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.radio -> setCurrentFragment(radioFragment)
+                R.id.tasbeeh -> setCurrentFragment(tasbeehFragment)
+                R.id.ahadeth -> setCurrentFragment(ahadethFragment)
+                R.id.quran -> setCurrentFragment(quranFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment, "")
+            .commit()
+    }
+
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        return super.onSupportNavigateUp() || findNavController(R.id.fragmentContainerView).navigateUp()
+//    }
+}
--- app/src/main/java/com/example/islamiapp/splash/SplashActivity.kt
+++ app/src/main/java/com/example/islamiapp/splash/SplashActivity.kt
@@ -1,9 +1,11 @@
-package com.example.islamiapp
+package com.example.islamiapp.splash
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.islamiapp.home.HomeActivity
+import com.example.islamiapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
